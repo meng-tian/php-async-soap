9fe45b8f826adbfa8f56a47306753fd0651f47aa
Meng Tian mac <tianmeng94@hotmail.com>
2016-03-01 22:52:02 +0000

    Add magic method __call.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # PHP Asynchronous SOAP
 Common interface for asynchronous SOAP client.
 
-[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/meng-tian/php-async-soap/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/meng-tian/php-async-soap/?branch=master)
-
--- src/SoapClientInterface.php
+++ src/SoapClientInterface.php
@@ -5,26 +5,35 @@ namespace Meng\AsyncSoap;
 interface SoapClientInterface
 {
     /**
-     * Synchronous SOAP call
+     * Magic method to simplify SOAP call. This method is asynchronous.
      *
-     * @param string $name
-     * @param array $arguments
-     * @param array $options
-     * @param mixed $inputHeaders
-     * @param array $output_headers
+     * @param string $name Operation name
+     * @param array $arguments Operation arguments
+     * @return Promise instance with a then method
+     */
+    public function __call($name, $arguments);
+
+    /**
+     * Synchronous SOAP call.
+     *
+     * @param string $name Operation name
+     * @param array $arguments Operation arguments
+     * @param array $options Options
+     * @param mixed $inputHeaders Input SOAP headers
+     * @param array $output_headers Output SOAP headers
      * @return mixed
      */
     public function call($name, array $arguments, array $options = null, $inputHeaders = null, array &$output_headers = null);
 
     /**
-     * Asynchronous SOAP call
+     * Asynchronous SOAP call.
      *
-     * @param string $name
-     * @param array $arguments
-     * @param array $options
-     * @param mixed $inputHeaders
-     * @param array $output_headers
-     * @return a promise instance with a then method
+     * @param string $name Operation name
+     * @param array $arguments Operation arguments
+     * @param array $options Options
+     * @param mixed $inputHeaders Input SOAP headers
+     * @param array $output_headers Output SOAP headers
+     * @return Promise instance with a then method
      */
     public function callAsync($name, array $arguments, array $options = null, $inputHeaders = null, array &$output_headers = null);
 }
